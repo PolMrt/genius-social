d1fc907c2972ac23b3a76c2656a4e60447b75bee
Pol Marnette <pol@hey.com>
2022-06-28 17:27:11 +0200

    [~] Fix cookie issue

--- apps/backend/src/backend.controller.ts
+++ apps/backend/src/backend.controller.ts
@@ -28,21 +28,21 @@ export class BackendController {
   async login(@Request() req: any, @Res({ passthrough: true }) res: Response) {
     const tokenData = await this.authService.login(req.user);
 
-    const expirationsoptions = {
+    const sharedOptions = {
       expires: new Date(new Date().getTime() + 24 * 60 * 60 * 1000), // 24h
       maxAge: 60 * 60 * 24,
+      domain: this.configService.get<string>("FRONTEND_DOMAIN"),
     };
 
     res.cookie("ACCESS-TOKEN", tokenData.access_token, {
-      ...expirationsoptions,
+      ...sharedOptions,
       sameSite: "strict",
       httpOnly: true,
-      domain: this.configService.get<string>("FRONTEND_DOMAIN"),
       secure: true,
     });
 
     res.cookie("XSRF-TOKEN", tokenData.xsrfToken, {
-      ...expirationsoptions,
+      ...sharedOptions,
     });
 
     return { user: tokenData.user };
