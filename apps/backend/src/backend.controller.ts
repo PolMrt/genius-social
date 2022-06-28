495a7752c29ab360fbfb3eea4e5fb72ed5ebd0a2
Pol Marnette <pol@hey.com>
2022-06-28 17:28:30 +0200

    [~] Fix cookie 2

--- apps/backend/src/backend.controller.ts
+++ apps/backend/src/backend.controller.ts
@@ -32,13 +32,13 @@ export class BackendController {
       expires: new Date(new Date().getTime() + 24 * 60 * 60 * 1000), // 24h
       maxAge: 60 * 60 * 24,
       domain: this.configService.get<string>("FRONTEND_DOMAIN"),
+      secure: true,
     };
 
     res.cookie("ACCESS-TOKEN", tokenData.access_token, {
       ...sharedOptions,
       sameSite: "strict",
       httpOnly: true,
-      secure: true,
     });
 
     res.cookie("XSRF-TOKEN", tokenData.xsrfToken, {
